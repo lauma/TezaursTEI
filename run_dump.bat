fbbc36663eb9923cbcacf649f605434be8bb10f5
lauma <lauma.pretkalnin@gmail.com>
2021-12-15 14:07:56 +0200

    Restrukturizācija + 100 vārdu saraksts.

--- run_dump.bat
+++ run_dump.bat
@@ -1,7 +1,7 @@
 REM Filtering by whitelist.
 REM Whitelist format: each line contains one allowed entryword and optional tab-separated homonym index.
-::python lv/ailab/teztei/tezaurs_dump.py mlvv_dv whitelist100.txt
-::python lv/ailab/teztei/tezaurs_dump.py tezaurs_dv whitelist100.txt
+::python lv/ailab/teztei/tezaurs_dump.py mlvv_dv config/whitelist100.txt
+::python lv/ailab/teztei/tezaurs_dump.py tezaurs_dv config/whitelist100.txt
 
 REM Dumping whole dictionary (mwe entries excluded).
 ::python lv/ailab/teztei/tezaurs_dump.py mlvv_dv
