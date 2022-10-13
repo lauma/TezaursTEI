d2ca3803eaca47b96714f9c1b57f5abbc4fed2cc
lauma <lauma.pretkalnin@gmail.com>
2022-10-13 20:47:03 +0300

    TEI bugfix

--- lv/ailab/tezdb/overview_querries.py
+++ lv/ailab/tezdb/overview_querries.py
@@ -21,7 +21,8 @@ SELECT e.id, type_id, name as type_name, heading, human_key, homonym_no,
     primary_lexeme_id, e.data->>'Etymology' as etym, e.data as data
 FROM {db_connection_info['schema']}.entries e
 JOIN {db_connection_info['schema']}.entry_types et ON e.type_id = et.id
-cORDER BY type_id, heading
+WHERE {where_clause} NOT e.hidden
+ORDER BY type_id, heading
 """
     cursor.execute(sql_entries)
     counter = 0
