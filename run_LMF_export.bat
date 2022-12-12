11985b1dfcc81dfe046de865fdbdc8de041066c5
lauma <lauma.pretkalnin@gmail.com>
2022-12-12 22:00:42 +0200

    Validation how-to.

--- run_LMF_export.bat
+++ run_LMF_export.bat
@@ -1,4 +1,7 @@
 REM Exporting Wordnet LMF from dictionary.
 ::python -m lv.ailab.tezlmf.do_lmf_wordnet_export tezaurs_2022_04
 ::python -m lv.ailab.tezlmf.do_lmf_wordnet_export tezaurs_dv
+
+REM Validation.
+::python -m wn validate tezaurs_dv_lmf.xml
 pause
