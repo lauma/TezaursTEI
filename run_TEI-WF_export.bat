1865f71ff7cd3ab2fe4b1b5594e546796cd46c4c
lauma <lauma.pretkalnina@gmail.com>
2025-07-20 00:29:12 +0300

    Skaidrojums.

--- run_TEI-WF_export.bat
+++ run_TEI-WF_export.bat
@@ -1,4 +1,4 @@
 chcp 65001
-REM Exporting REM Exporting filtered list of wordforms in ispell format.
+REM Exporting unfiltered list of wordforms in TEI format.
 python -m lv.ailab.teztei.do_tei_wordform_export tezaurs_2025_03
 pause
